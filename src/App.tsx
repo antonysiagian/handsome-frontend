e0c0cf328543abf30ca3b20d64062fdec51d1e6d
Antony Siagian <antonysiagian@Antonys-MacBook-Air.local>
2026-06-08 09:56:07 +1000

    feat: change title

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <h1 className="text-4xl font-bold">Hello World</h1>
+      <h1 className="text-4xl font-bold">Hello World, Be kind</h1>
     </div>
   )
 }
